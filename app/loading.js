62cd398278f5e905017d351defb8d642548740b8
codebycc <ciici6440@gmail.com>
2025-03-18 09:14:11 +0000

    made changes in the app

---
+++ app/loading.js
@@ -0,0 +1,12 @@
+import Spinner from "./components/Spinner";
+
+function loading() {
+  return (
+    <div className="">
+      <Spinner />
+      {/* <h1>loading...</h1> */}
+    </div>
+  );
+}
+
+export default loading;
--- app/products/page.js
+++ app/products/page.js
@@ -12,7 +12,7 @@ export const revalidate = 0;
 async function page() {
   const products = await getProducts();
 
-  console.log(products);
+  // console.log(products);
   return (
     <div className="flex justify-center items-center mt-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
