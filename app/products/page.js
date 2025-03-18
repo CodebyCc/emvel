01bfd3377b21d66caef834384611b5b332c40cc6
codebycc <ciici6440@gmail.com>
2025-03-18 10:14:57 +0000

    Added a margin to the page contaier

--- app/products/page.js
+++ app/products/page.js
@@ -14,7 +14,7 @@ async function page() {
 
   // console.log(products);
   return (
-    <div className="flex justify-center items-center mt-8">
+    <div className="flex justify-center items-center mt-8 mb-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map((product, index) => (
           <Card key={index} product={product} />
