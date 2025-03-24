4268289a4c3c7a3be9eeac126c32aa98ed192262
codebycc <ciici6440@gmail.com>
2025-03-24 15:23:57 +0000

    Added a few animation

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Buttons from "./components/Buttons";
 export default function Home() {
   return (
     <div className="py-6 px-4">
-      <div className="max-w-4xl mx-auto px-6 py-12 text-center flex flex-col justify-center">
+      <div className="max-w-4xl mx-auto px-6 py-12 text-center flex flex-col justify-center animate-slideUp">
         <h1 className="text-4xl md:text-6xl font-bold mb-4">
           Welcome to
           <span className="text-green-400"> Emvel Royal Company</span>
@@ -14,6 +14,13 @@ export default function Home() {
           <span className="text-green-400"> 𝐓𝐇𝐄 BEST HEALTHCARE</span>
         </p>
         <Buttons />
+
+        {/* <div className="bg-green-500 p-4 text-white text-center animate-fadeIn">
+          Fade In Animation
+        </div>
+        <div className="bg-red-500 p-4 text-white text-center animate-slideUp">
+          Slide Up Animation
+        </div> */}
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,20 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        fadeIn: "fadeIn 1s ease-in-out",
+        slideUp: "slideUp 0.5s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        slideUp: {
+          "0%": { transform: "translateY(20px)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+      },
     },
   },
   plugins: [],
